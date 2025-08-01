dc4e25cc4cc956bb6b9864fadacd68f819465d9b
ericcclin <141807050+ericcclin@users.noreply.github.com>
2025-08-01 10:22:52 +0800

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+#Root Configuration
+variable "gcp_project" {
+  type = string
+  description = "Default GCP Project"
+}
+
+
+variable "gcp_region" {
+  type = string
+  description = "Default Region"
+}
