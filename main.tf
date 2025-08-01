f400b357b0b9ada8bb85ef2af836efe20cb472ab
ericcclin <141807050+ericcclin@users.noreply.github.com>
2025-08-01 10:26:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@
       project = var.gcp_project // Using a variable for project, or can be picked from GOOGLE_PROJECT env var
       region  = var.gcp_region  // Using a variable for region, or can be picked from GOOGLE_REGION env var
     }
+
+
+    resource "google_storage_bucket" "my_gcs_bucket" {
+      name     = "my-first-bucket-ericlin-tf" # Replace with a globally unique bucket name
+      location = "ASIA"                     # Replace with your desired location (e.g., "US", "EU", "ASIA")
+      # Optional: Add other configurations like storage class, versioning, etc.
+      storage_class = "STANDARD"
+      versioning {
+        enabled = true
+      }
+    }
