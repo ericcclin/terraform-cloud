4946c34f6dc1eb75f23c634110b905391989c064
ericcclin <141807050+ericcclin@users.noreply.github.com>
2025-08-03 13:39:37 +0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,4 +8,18 @@ variable "gcp_project" {
 variable "gcp_region" {
   type = string
   description = "Default Region"
+  default     = "asia-east2"
 }
+
+variable "bucket_name" {
+  description = "The desired base name for the GCS bucket. A random suffix will be added."
+  type        = string
+  default     = "my-unique-tf-bucket"
+}
+
+variable "bucket_location" {
+  description = "The location for the GCS bucket (e.g., US, EU, ASIA)."
+  type        = string
+  default     = "ASIA"
+}
+
